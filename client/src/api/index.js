bf02ec1bf6294c25a7fcae4b74f413fb43f4e81b
kasularevanth <kasularevanth1@gmail.com>
2025-04-10 20:48:38 +0530

    Updated getAllProducts

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-zara.onrender.com",
+  baseURL: "http://localhost:8080/api/",
 });
 
 export const UserSignUp = async (data) => await API.post("/user/signup", data);
